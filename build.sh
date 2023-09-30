16319999f8d7b52fc622fd71198325b0928fdf34
Paulius Velesko <pvelesko@pglc.io>
2023-09-30 13:53:12 -0500

    wip

--- build.sh
+++ build.sh
@@ -8,24 +8,24 @@ INSTALL_DIR=/home/pvelesko/space/install/intel-test
 # get todays date in format 2023.09.30
 DATE=$(date +%Y.%m.%d)
 
-BUILD_TOOL="Unix Makefiles"
+# BUILD_TOOL=" -G Unix Makefiles"
 #BUILD_TOOL="Ninja"
 
 # libgmm depends on 
-cmake -G ${BUILD_TOOL} -S gmmlib -B gmmlib/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/gmmlib
-cmake -G ${BUILD_TOOL} --build gmmlib/build --config Release -j $(nproc) -G $
-cmake -G ${BUILD_TOOL} --build gmmlib/build --target install
+cmake ${BUILD_TOOL} -S gmmlib -B gmmlib/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/gmmlib
+cmake ${BUILD_TOOL} --build gmmlib/build --config Release
+cmake ${BUILD_TOOL} --build gmmlib/build --target install -j $(nproc)
 
-cmake -G ${BUILD_TOOL} -S igsc -B igsc/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igsc
-cmake -G ${BUILD_TOOL} --build igsc/build --config Release -j $(nproc)
-cmake -G ${BUILD_TOOL} --build igsc/build --target install
+cmake ${BUILD_TOOL} -S igsc -B igsc/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igsc
+cmake ${BUILD_TOOL} --build igsc/build --config Release
+cmake ${BUILD_TOOL} --build igsc/build --target install -j $(nproc)
 
 git clone -b ocl-open-140 https://github.com/intel/opencl-clang llvm-project/llvm/projects/opencl-clang
 git clone -b llvm_release_140 https://github.com/KhronosGroup/SPIRV-LLVM-Translator llvm-project/llvm/projects/llvm-spirv
-cmake -G ${BUILD_TOOL} -S igc -B igc/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igc/$DATE -G
-cmake -G ${BUILD_TOOL} --build igc/build --config Release -j $(nproc)
-cmake -G ${BUILD_TOOL} --build igc/build --target install
+cmake ${BUILD_TOOL} -S igc -B igc/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igc/$DATE
+cmake ${BUILD_TOOL} --build igc/build --config Release
+cmake ${BUILD_TOOL} --build igc/build --target install  -j $(nproc)
 
-cmake -G ${BUILD_TOOL} -S neo -B neo/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/neo/$DATE -DGMM_DIR=/opt/install/intel/gmmlib -DCMAKE_PREFIX_PATH=${INSTALL_DIR} -DSKIP_UNIT_TESTS=ON -DOCL_ICD_VENDORDIR=${INSTALL_DIR}
-cmake -G ${BUILD_TOOL} --build neo/build --config Release -j $(nproc)
-cmake -G ${BUILD_TOOL} --build neo/build --target install
+cmake ${BUILD_TOOL} -S neo -B neo/build -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/neo/$DATE -DGMM_DIR=/opt/install/intel/gmmlib -DCMAKE_PREFIX_PATH=${INSTALL_DIR} -DSKIP_UNIT_TESTS=ON -DOCL_ICD_VENDORDIR=${INSTALL_DIR}
+cmake ${BUILD_TOOL} --build neo/build --config Release
+cmake ${BUILD_TOOL} --build neo/build --target install -j $(nproc)
