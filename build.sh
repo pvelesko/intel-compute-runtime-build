a80d65fb09c493019896d96e8490ac9201de3b47
Paulius Velesko <pvelesko@pglc.io>
2023-09-30 05:54:14 -0500

    WIP

--- build.sh
+++ build.sh
@@ -14,6 +14,8 @@ cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igsc  -S igsc -B igsc/build
 cmake --build igsc/build --config Release -j $(nproc)
 cmake --build igsc/build --target install
 
+git clone -b ocl-open-140 https://github.com/intel/opencl-clang llvm-project/llvm/projects/opencl-clang
+git clone -b llvm_release_140 https://github.com/KhronosGroup/SPIRV-LLVM-Translator llvm-project/llvm/projects/llvm-spirv
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igc  -S igc -B igc/build
 cmake --build igc/build --config Release -j $(nproc)
 cmake --build igc/build --target install
