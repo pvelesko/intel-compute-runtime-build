aa6b537e39f471e828738715f56bf8ded9dcc5d8
Paulius Velesko <pvelesko@pglc.io>
2023-09-30 05:19:56 -0500

    add submodules

---
+++ build.sh
@@ -0,0 +1,19 @@
+# Intel Compute Runtime Depends on:
+# 1. Intel(R) Graphics Compiler (igc)
+# 2. Intel(R) Graphics Memory Management Library (gmmlib)
+# 3. Intel(R) Graphics System Controller Firmware Update Library (igsc)
+
+INSTALL_DIR=/home/pvelesko/space/install/intel-test
+
+# libgmm depends on 
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/gmmlib  -S gmmlib -B gmmlib/build
+cmake --build gmmlib/build --config Release -j $(nproc)
+cmake --build gmmlib/build --target install
+
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igsc  -S igsc -B igsc/build
+cmake --build igsc/build --config Release -j $(nproc)
+cmake --build igsc/build --target install
+
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/igc  -S igc -B igc/build
+cmake --build igc/build --config Release -j $(nproc)
+cmake --build igc/build --target install
