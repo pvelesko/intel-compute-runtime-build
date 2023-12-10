44897ee7fb768a2afb0a704037ffddff5b7b434b
Paulius Velesko <pvelesko@pglc.io>
2023-12-10 06:05:48 -0600

    wip

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [ $# -eq 0 ]; then
   echo "  --download                  Download all dependencies"
   echo "  --clean                     Clean all dependencies"
   echo "  --build <install path>      Configure and build everything with CMAKE_INSTALL_PREFIX=<install path>"
-  echo "  --modulefiles               Generate modulefiles, requires --build"
+  echo "  --modulefiles               Generate modulefiles"
   echo "  --igc-tag <tag>             Specify the IGC tag"
   echo "  --neo-tag <tag>             Specify the NEO tag"
   echo "  -h, --help                  Show this help message"
@@ -44,10 +44,6 @@ while [ "$#" -gt 0 ]; do
       fi
       ;;
     --modulefiles)
-      if [ "$BUILD" = false ]; then
-      echo "Build is set to false. Exiting..."
-      exit 1
-      fi
       MODULEFILES=true
       ;;
     --igc-tag)
@@ -119,13 +115,13 @@ checkout_tags() {
   OCL_ICD_INSTALL_DIR=${INSTALL_DIR}/opencl
 
   #dump install dir vars to cache.txt
-  echo "METEE_INSTALL_DIR=${METEE_INSTALL_DIR}" > cache.txt
-  echo "GMMLIB_INSTALL_DIR=${GMMLIB_INSTALL_DIR}" >> cache.txt
-  echo "IGSC_INSTALL_DIR=${IGSC_INSTALL_DIR}" >> cache.txt
-  echo "IGC_INSTALL_DIR=${IGC_INSTALL_DIR}" >> cache.txt
-  echo "NEO_INSTALL_DIR=${NEO_INSTALL_DIR}" >> cache.txt
-  echo "LEVEL_ZERO_INSTALL_DIR=${LEVEL_ZERO_INSTALL_DIR}" >> cache.txt
-  echo "OCL_ICD_INSTALL_DIR=${OCL_ICD_INSTALL_DIR}" >> cache.txt
+  echo "METEE_INSTALL_DIR=${METEE_INSTALL_DIR}" | tee cache.txt
+  echo "GMMLIB_INSTALL_DIR=${GMMLIB_INSTALL_DIR}" | tee -a cache.txt
+  echo "IGSC_INSTALL_DIR=${IGSC_INSTALL_DIR}" | tee -a cache.txt
+  echo "IGC_INSTALL_DIR=${IGC_INSTALL_DIR}" | tee -a cache.txt
+  echo "NEO_INSTALL_DIR=${NEO_INSTALL_DIR}" | tee -a cache.txt
+  echo "LEVEL_ZERO_INSTALL_DIR=${LEVEL_ZERO_INSTALL_DIR}" | tee -a cache.txt
+  echo "OCL_ICD_INSTALL_DIR=${OCL_ICD_INSTALL_DIR}" | tee -a cache.txt
 }
 
 if [ $DOWNLOAD ]; then
@@ -149,14 +145,14 @@ if [ $CLEAN ]; then
     echo "Cleaning all dependencies"
     # find and remove all CMakeLists.txt here
     find . -name "CMakeLists.txt" -type f -delete
-    cd gmmlib; git clean -fdx; rm -rf ./build; cd ../
-    cd igsc; git clean -fdx; rm -rf ./build; cd ../
-    cd vc-intrinsics; git clean -fdx; rm -rf ./build; cd ../
-    # cd llvm-project; git clean -fdx; rm -rf ./build; cd ../
-    # cd SPIRV-Tools; git clean -fdx; rm -rf ./build; cd ../
-    # cd SPIRV-Headers; git clean -fdx; rm -rf ./build; cd ../
-    cd igc; git clean -fdx; rm -rf ./build; cd ../
-    cd neo; git clean -fdx; rm -rf ./build; cd ../
+    # cd gmmlib; git clean -fdx; rm -rf ./build; cd ../
+    # cd igsc; git clean -fdx; rm -rf ./build; cd ../
+    # cd vc-intrinsics; git clean -fdx; rm -rf ./build; cd ../
+    # # cd llvm-project; git clean -fdx; rm -rf ./build; cd ../
+    # # cd SPIRV-Tools; git clean -fdx; rm -rf ./build; cd ../
+    # # cd SPIRV-Headers; git clean -fdx; rm -rf ./build; cd ../
+    # cd igc; git clean -fdx; rm -rf ./build; cd ../
+    # cd neo; git clean -fdx; rm -rf ./build; cd ../
 fi
 
 # Can't just configure in one step and then build because configuration requires built dependencies
@@ -166,36 +162,51 @@ if [ $BUILD ]; then
     echo "Building all dependencies"
     echo "Setting CC=gcc CXX=g++"
 
-    pip install mako
-    sudo apt install libllvmspirvlib-14-dev llvm-spirv-14 llvm-14 llvm-14-dev clang-14 liblld-14 liblld-14-dev
+    # pip install mako
+    # sudo apt install libllvmspirvlib-14-dev llvm-spirv-14 llvm-14 llvm-14-dev clang-14 liblld-14 liblld-14-dev
 
-    rm -f metee/build/CMakeCache.txt
-    CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S metee -B metee/build -DCMAKE_INSTALL_PREFIX=${METEE_INSTALL_DIR}
-    cmake  --build metee/build --config Release -j $(nproc)
-    cmake  --build metee/build --target install -j $(nproc)
+    # check if installed, if not install
+    if [ ! -d ${METEE_INSTALL_DIR} ]; then
+      rm -f metee/build/CMakeCache.txt
+      CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S metee -B metee/build -DCMAKE_INSTALL_PREFIX=${METEE_INSTALL_DIR}
+      cmake  --build metee/build --config Release -j $(nproc)
+      cmake  --build metee/build --target install -j $(nproc)
+    fi 
 
-    CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S gmmlib -B gmmlib/build -DCMAKE_INSTALL_PREFIX=${GMMLIB_INSTALL_DIR}
-    cmake --build gmmlib/build --config Release -j $(nproc)
-    cmake --build gmmlib/build --target install -j $(nproc)
+    if [ ! -d ${GMMLIB_INSTALL_DIR} ]; then
+      rm -f gmmlib/build/CMakeCache.txt
+      CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S gmmlib -B gmmlib/build -DCMAKE_INSTALL_PREFIX=${GMMLIB_INSTALL_DIR}
+      cmake --build gmmlib/build --config Release -j $(nproc)
+      cmake --build gmmlib/build --target install -j $(nproc)
+    fi
 
-    CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S igsc -B igsc/build -DCMAKE_INSTALL_PREFIX=${IGSC_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${METEE_INSTALL_DIR}
-    cmake --build igsc/build --config Release -j $(nproc)
-    cmake --build igsc/build --target install -j $(nproc)
+    if [ ! -d ${IGSC_INSTALL_DIR} ]; then
+      rm -f igsc/build/CMakeCache.txt
+      CC=gcc CXX=g++ cmake -G "${BUILD_TOOL}" -S igsc -B igsc/build -DCMAKE_INSTALL_PREFIX=${IGSC_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${METEE_INSTALL_DIR}
+      cmake --build igsc/build --config Release -j $(nproc)
+      cmake --build igsc/build --target install -j $(nproc)
+    fi
 
-    cmake -G "${BUILD_TOOL}" -S igc -B igc/build -DCMAKE_INSTALL_PREFIX=${IGC_INSTALL_DIR} -DIGC_OPTION__SPIRV_TOOLS_MODE=Prebuilds -DIGC_OPTION__USE_PREINSTALLED_SPIRV_HEADERS=ON -DIGC_OPTION__LLVM_PREFERRED_VERSION=14.0.0
-    cmake --build igc/build --config Release -j $(nproc)
-    cmake --build igc/build --target install  -j $(nproc)
+    if [ ! -d ${IGC_INSTALL_DIR} ]; then
+      rm -f igc/build/CMakeCache.txt
+      cmake -G "${BUILD_TOOL}" -S igc -B igc/build -DCMAKE_INSTALL_PREFIX=${IGC_INSTALL_DIR} -DIGC_OPTION__SPIRV_TOOLS_MODE=Prebuilds -DIGC_OPTION__USE_PREINSTALLED_SPIRV_HEADERS=ON -DIGC_OPTION__LLVM_PREFERRED_VERSION=14.0.0
+      cmake --build igc/build --config Release -j $(nproc)
+      cmake --build igc/build --target install  -j $(nproc)
+    fi
 
-    cmake -G "${BUILD_TOOL}" -S level-zero -B level-zero/build -DCMAKE_INSTALL_PREFIX=${LEVEL_ZERO_INSTALL_DIR} 
-    cmake --build level-zero/build --config Release -j $(nproc)
-    cmake --build level-zero/build --target install  -j $(nproc)
+    if [ ! -d ${LEVEL_ZERO_INSTALL_DIR} ]; then
+      rm -f level-zero/build/CMakeCache.txt
+      cmake -G "${BUILD_TOOL}" -S level-zero -B level-zero/build -DCMAKE_INSTALL_PREFIX=${LEVEL_ZERO_INSTALL_DIR} 
+      cmake --build level-zero/build --config Release -j $(nproc)
+      cmake --build level-zero/build --target install  -j $(nproc)
+    fi
 
-    cmake -G "${BUILD_TOOL}" -S neo -B neo/build -DCMAKE_INSTALL_PREFIX=${NEO_INSTALL_DIR} -DIGC_DIR=${IGC_INSTALL_DIR} -DGMM_DIR=${GMMLIB_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${IGSC_INSTALL_DIR} -DSKIP_UNIT_TESTS=ON -DOCL_ICD_VENDORDIR=${OCL_ICD_INSTALL_DIR} -DLevelZero_INCLUDE_DIR=${LEVEL_ZERO_INSTALL_DIR}/include
-    cmake --build neo/build --config Release -j $(nproc)
-    cmake --build neo/build --target install -j $(nproc)
-
-
-
+    if [ ! -d ${NEO_INSTALL_DIR} ]; then
+      rm  -f neo/build/CMakeCache.txt
+      cmake -G "${BUILD_TOOL}" -S neo -B neo/build -DCMAKE_INSTALL_PREFIX=${NEO_INSTALL_DIR} -DIGC_DIR=${IGC_INSTALL_DIR} -DGMM_DIR=${GMMLIB_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${IGSC_INSTALL_DIR} -DSKIP_UNIT_TESTS=ON -DOCL_ICD_VENDORDIR=${NEO_INSTALL_DIR}/share/OpenCL/vendors -DLevelZero_INCLUDE_DIR=${LEVEL_ZERO_INSTALL_DIR}/include
+      cmake --build neo/build --config Release -j $(nproc)
+      cmake --build neo/build --target install -j $(nproc)
+    fi
 fi
 
 
